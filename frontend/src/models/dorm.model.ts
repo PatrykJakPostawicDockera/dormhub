75e9ef702de3b4b911604aeb41552db8a397f04b
matt-pasek <m_pasek@icloud.com>
2024-05-24 02:16:10 +0300

    dorm model

---
+++ frontend/src/models/dorm.model.ts
@@ -0,0 +1,9 @@
+export default interface Dorm {
+  Address: string;
+  ApartmentAmount: number;
+  DormCode: string;
+  DormId: string;
+  DormVacancy: number;
+  ImageUrl: string;
+  Name: string;
+}
