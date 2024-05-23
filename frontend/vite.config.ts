e6f510d5240f3c89d1d01669612d7c49b1182e2a
matt-pasek <m_pasek@icloud.com>
2024-05-23 14:23:55 +0300

    new icons, removed unnecessary files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,29 @@ export default defineConfig({
         short_name: 'DormHub',
         description: 'DormHub - Hub for Your Dormitory',
         theme_color: '#000000',
-        icons: [],
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
       },
     }),
   ],
