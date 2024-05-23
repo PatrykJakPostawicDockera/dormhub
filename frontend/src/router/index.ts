30cc7cbbe93e90873c535e5f97261e1458b5daa2
matt-pasek <m_pasek@icloud.com>
2024-05-24 00:50:39 +0300

    added todos view to router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -43,6 +43,11 @@ const router = createRouter({
           component: () => import('../views/PostsView.vue'),
         },
         {
+          path: 'todos',
+          name: 'Todos',
+          component: () => import('../views/TodosView.vue'),
+        },
+        {
           path: 'guests',
           name: 'Guests',
           component: () => import('../views/GuestsView.vue'),
